d5744cd368a23c896cc84838b3cacba98f844141
Ann K <64134468+AnnaCVrecruitment@users.noreply.github.com>
2022-10-22 00:32:17 +0300

    Создание объектов

--- js/main.js
+++ js/main.js
@@ -1,4 +1,55 @@
-/*eslint-disable no-alert*/
+const SIMILAR_POST_COUNT = 5;
+
+let getRandomIntInclusive = (min, max) => {
+  if (min < 0) {
+    alert('Поменяйте первый параметр, значение минимума');
+    return NaN;
+  }
+  if (max < 0) {
+    alert('Поменяйте второй параметр, значение максимума');
+    return NaN;
+  }
+  if (max <= min) {
+    alert('Поменяйте один из параметров, чтобы минимум был меньше максимума');
+    return NaN;
+  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  result = Math.random() * (max - min + 1) + min;
+  return Math.floor(result);
+};
+
+let createPost = () => {
+  
+  return {
+    id: getRandomIntInclusive(1, 25),
+    url: "photos/" + getRandomIntInclusive(1, 25) + ".jpg",
+    description:'',
+    likes: getRandomIntInclusive(15, 200),
+    comments: getRandomIntInclusive(0, 200),
+  };
+};
+
+const similarPosts = Array.from({length: SIMILAR_POST_COUNT}, createPost);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*eslint-disable no-alert
 const getRandomIntInclusive = (min, max) => {
   if (min < 0) {
     alert('Поменяйте первый параметр, значение минимума');
@@ -14,9 +65,10 @@ const getRandomIntInclusive = (min, max) => {
   }
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor((Math.random() * (max - min + 1) + min);
 };
 getRandomIntInclusive(1,10);
+
 
 const getCommentlength = (commentString, mnLen, mxLen) => {
   if (typeof mnLen !== 'number' && typeof mxLen !== 'number' && typeof commentString !== 'string' && commentString.length < mnLen || commentString.length > mxLen) {
@@ -28,7 +80,7 @@ const getCommentlength = (commentString, mnLen, mxLen) => {
 };
 
 getCommentlength('мопс',2,4);
-/*
+
 https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 function getRandomIntInclusive(min, max) {
@@ -37,4 +89,5 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
   http://kodesource.top/javascript/form/string-length.php
+
 */
